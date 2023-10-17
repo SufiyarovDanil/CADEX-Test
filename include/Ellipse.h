d1d47dd9b9ef7155c429ad7fa77db0b9fb451403
SufiyarovDanil <danil-sufiyarov@mail.ru>
2023-10-17 14:26:11 +0500

    добавил конструктор в класс Ellipse

--- include/Ellipse.h
+++ include/Ellipse.h
@@ -16,6 +16,8 @@ namespace Curve
 
 	public:
 
+		Ellipse(const float x_radius_axis, const float y_radius_axis);
+
 		virtual Vec3 GetPoint3D(const float t) const override;
 
 		virtual Vec3 GetFirstDerivative(const float t) const override;
--- src/Ellipse.cpp
+++ src/Ellipse.cpp
@@ -1,8 +1,20 @@
 #include "Ellipse.h"
+#include <iostream>
 
 
 namespace Curve
 {
+	Ellipse::Ellipse(const float x_radius_axis, const float y_radius_axis)
+		:
+		x_radius_axis(x_radius_axis),
+		y_radius_axis(y_radius_axis)
+	{
+		if (x_radius_axis < 0.f || y_radius_axis < 0.f)
+		{
+			throw std::runtime_error("radius radii must be positive");
+		}
+	}
+
 	Vec3 Ellipse::GetPoint3D(const float t) const
 	{
 		return Vec3(0.f, 0.f, 0.f);
